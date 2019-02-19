e79eb8433505a9731927f01e4cfb37e39d0b0b30
Tom <tom.clement@bizboard.nl>
2019-02-19 09:10:29 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ecmtool',
-    version='0.1.1',
+    version='0.1.3',
     packages=['ecmtool'],
     install_requires=['numpy',
                       'scipy',
