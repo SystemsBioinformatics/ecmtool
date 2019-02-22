e84ae0f10d69740904c9dadc38f48ce5580aa88b
Tom <tom.clement@bizboard.nl>
2019-02-22 14:39:23 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ecmtool',
-    version='0.1.4',
+    version='0.1.5',
     packages=['ecmtool'],
     install_requires=['numpy',
                       'scipy',
