642b8b06f4f81c6484f0fe98764c4ae995d74555
dhdegroot <d.h.de.groot@vu.nl>
2020-04-23 19:49:08 +0200

    Update helpers_analyze_results.py

--- results_analysis/helpers_analyze_results.py
+++ results_analysis/helpers_analyze_results.py
@@ -8,19 +8,19 @@ def relative_path(file_path):
     return os.path.join(os.path.dirname(__file__), file_path)
 
 
-def get_efms(N, reversibility, verbose=True):
-   import matlab.engine
-   engine = matlab.engine.start_matlab()
-   engine.cd(relative_path('efmtool'))
-   result = engine.CalculateFluxModes(matlab.double([list(row) for row in N]), matlab.logical(reversibility))
-   if verbose:
-       print('Fetching calculated EFMs')
-   size = result['efms'].size
-   shape = size[1], size[0] # _data is in transposed form w.r.t. the result matrix
-   efms = np.reshape(np.array(result['efms']._data), shape)
-   if verbose:
-       print('Finishing fetching calculated EFMs')
-   return efms
+def get_efms(N, reversibility, verbose=True, efmtool_path='C:\\Users\\Daan\\surfdrive\\PhD\\Software\\efmtool'):
+    import matlab.engine
+    engine = matlab.engine.start_matlab()
+    engine.cd(efmtool_path)
+    result = engine.CalculateFluxModes(matlab.double([list(row) for row in N]), matlab.logical(reversibility))
+    if verbose:
+        print('Fetching calculated EFMs')
+    size = result['efms'].size
+    shape = size[1], size[0] # _data is in transposed form w.r.t. the result matrix
+    efms = np.reshape(np.array(result['efms']._data), shape)
+    if verbose:
+        print('Finishing fetching calculated EFMs')
+    return efms
 
 
 def check_bijection_Erik(ecms_first, ecms_second, network):
