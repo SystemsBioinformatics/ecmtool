6807e45508b18c83e34519b862b5db01905a5309
Erik Baalhuis <ebbaalhuis@gmail.com>
2020-07-27 16:21:32 +0200

    move cythonize out of setup.py for pip release

--- cython_setup.py
+++ cython_setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 import numpy as np
 from distutils.extension import Extension
+from Cython.Build import cythonize
 
 setup(
     name='ecmtool',
@@ -13,6 +14,7 @@ setup(
                       'cbmpy',
                       'psutil',
                       'sklearn'],
+    ext_modules=cythonize(Extension("_bglu_dense", ["ecmtool/_bglu_dense.pyx"], include_dirs=[np.get_include()]), compiler_directives={'language_level' : "3"}),
     include_dirs=[np.get_include()],
     url='https://github.com/tjclement/ecmtool',
     license='MIT',
