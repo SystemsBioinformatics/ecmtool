73a17c735d155ce679dd39cb5ec1db8ff400fba2
dhdegroot <daanhugodegroot@gmail.com>
2021-04-25 01:52:21 +0200

    Made normalize columns for large matrices faster

--- ecmtool/network.py
+++ ecmtool/network.py
@@ -730,7 +730,7 @@ class Network:
             if np.min(N_int.shape) == 0:
                 break
             else:
-                nullspace_int = null_space(normalize_columns(N_int))  # TODO: See if you can make normalize_columns faster
+                nullspace_int = null_space(normalize_columns(N_int))
             reacs_notin_nullspace = np.where(np.max(np.abs(nullspace_int), axis=1) < 1e-8)[0]
             if len(reacs_notin_nullspace):
                 removable_reactions.extend(reacs_notin_nullspace)
