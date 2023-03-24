874ee4ba8c742776673a59ac78b7d8c0e28eb4d1
dhdegroot <daanhugodegroot@gmail.com>
2023-03-24 09:14:48 +0100

    Update test_ecmtool.py

--- test_ecmtool.py
+++ test_ecmtool.py
@@ -5,33 +5,70 @@ import os
 import csv
 from dataclasses import dataclass
 
+outputfile = 'test_conversions.csv'
 
 @dataclass
 class ConversionsOutput:
-    outputfile = 'test_conversions.csv'
     ecoliCoreOutput = None
     ecoliCoreReturnCode = None
     conversions = None
     metabIds = None
+
+    def __init__(self, commandList):
+        # Run ecmtool on e coli core network
+        if os.path.isfile(outputfile):
+            os.remove(outputfile)
+        self.ecoliCoreOutput = subprocess.run(commandList, stdout=subprocess.PIPE, text=True)
+
+        self.ecoliCoreReturnCode = self.ecoliCoreOutput.returncode
+        with open(outputfile, newline='') as f:
+            reader = csv.reader(f)
+            self.metabIds = next(reader)
+        self.conversions = np.loadtxt(outputfile, delimiter=',', skiprows=1)
+
+    def normalize_conversions(self, true_metabIds):
+        # Order metabolites in the same way for newly found conversions
+        trueMetabIndices = np.array([true_metabIds.index(metab) for metab in self.metabIds])
+        conversions_loc = self.conversions.copy()
+        conversions_loc[:, trueMetabIndices]
+
+        # Normalise conversions such that all conversions sum up to 1
+        conversions_loc /= np.sum(conversions_loc, axis=1, keepdims=True)
+
+        # Order conversions based on first metabolite, then on second, etc,...
+        # argsort = np.lexsort(tuple(map(tuple, conversions_loc.T)))
+        # conversions_loc = conversions_loc[argsort, :]
+        return conversions_loc
+
+
+def get_metabs(filename):
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        true_metabIds = next(reader)
+    return true_metabIds
+
+
+def areConversionSetsEqual(conversions1, conversions2, atol=1e-8, rtol=1e-5):
+    ub = atol + rtol * np.abs(conversions2)
+    nMetabs = conversions2.shape[1]
+    for conv in conversions1:
+        closenessTF = np.abs(conversions2 - conv) <= ub
+        indsMatch = np.where(np.sum(closenessTF, axis=1) == nMetabs)[0]
+        if len(indsMatch) != 1:
+            return False
+        else:
+            conversions2 = np.delete(conversions2, indsMatch[0], 0)
+            ub = np.delete(ub, indsMatch[0], 0)
+    return True
 
 
 class TestEcolicore:
     @pytest.fixture(scope='class', autouse=True)
     def runEcolicore(self):
         # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
-        # Run ecmtool on e coli core network
-        ecoliOutput = ConversionsOutput()
-        if os.path.isfile(ecoliOutput.outputfile):
-            os.remove(ecoliOutput.outputfile)
         commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
-                       '--out_path', ecoliOutput.outputfile]
-        ecoliOutput.ecoliCoreOutput = subprocess.run(commandList, stdout=subprocess.PIPE, text=True)
-
-        ecoliOutput.ecoliCoreReturnCode = ecoliOutput.ecoliCoreOutput.returncode
-        with open(ecoliOutput.outputfile, newline='') as f:
-            reader = csv.reader(f)
-            ecoliOutput.metabIds = next(reader)
-        ecoliOutput.conversions = np.loadtxt(ecoliOutput.outputfile, delimiter=',', skiprows=1)
+                       '--out_path', outputfile]
+        ecoliOutput = ConversionsOutput(commandList)
         return ecoliOutput
 
     def test_ecolicoreRunCompleted(self, runEcolicore):
@@ -45,30 +82,51 @@ class TestEcolicore:
 
     def test_metabIds(self, runEcolicore):
         trueFilename = os.path.join(os.getcwd(), 'true_test_results', 'true_ecolicore_conversions.csv')
-        with open(trueFilename, newline='') as f:
-            reader = csv.reader(f)
-            true_metabIds = next(reader)
+        true_metabIds = get_metabs(trueFilename)
         assert (sorted(true_metabIds) == sorted(runEcolicore.metabIds))
 
     def test_conversions(self, runEcolicore):
         trueFilename = os.path.join(os.getcwd(), 'true_test_results', 'true_ecolicore_conversions.csv')
-        with open(trueFilename, newline='') as f:
-            reader = csv.reader(f)
-            true_metabIds = next(reader)
+        true_metabIds = get_metabs(trueFilename)
 
         # These true conversions are normalised such that each row sums to 1.
         true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
+        # Do the same for the current conversions
+        conversions_loc = runEcolicore.normalize_conversions(true_metabIds)
 
-        # Order metabolites in the same way for newly found conversions
-        trueMetabIndices = np.array([true_metabIds.index(metab) for metab in runEcolicore.metabIds])
-        conversions_loc = runEcolicore.conversions.copy()
-        conversions_loc[:, trueMetabIndices]
+        assert areConversionSetsEqual(conversions_loc, true_conversions)
 
-        # Normalise conversions such that all conversions sum up to 1
-        conversions_loc /= np.sum(conversions_loc, axis=1, keepdims=True)
 
-        # Order conversions based on first metabolite, then on second, etc,...
-        argsort = np.lexsort(tuple(map(tuple, conversions_loc.T)))
-        conversions_loc = conversions_loc[argsort, :]
+class TestEcolicoreDirect:
+    @pytest.fixture(scope='class', autouse=True)
+    def runEcolicore(self):
+        # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
+        commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
+                       '--out_path', outputfile, '--direct', 'True']
+        ecoliOutput = ConversionsOutput(commandList)
+        return ecoliOutput
 
-        assert (np.allclose(conversions_loc, true_conversions, rtol=1e-05, atol=1e-08))
+    def test_ecolicoreRunCompleted(self, runEcolicore):
+        assert (runEcolicore.ecoliCoreReturnCode == 0)
+
+    def test_numberECMs(self, runEcolicore):
+        assert (runEcolicore.conversions.shape[0] == 689)
+
+    def test_numberMetabs(self, runEcolicore):
+        assert (len(runEcolicore.metabIds) == 21)
+
+    def test_metabIds(self, runEcolicore):
+        trueFilename = os.path.join(os.getcwd(), 'true_test_results', 'true_ecolicore_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+        assert (sorted(true_metabIds) == sorted(runEcolicore.metabIds))
+
+    def test_conversions(self, runEcolicore):
+        trueFilename = os.path.join(os.getcwd(), 'true_test_results', 'true_ecolicore_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+
+        # These true conversions are normalised such that each row sums to 1.
+        true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
+        # Do the same for the current conversions
+        conversions_loc = runEcolicore.normalize_conversions(true_metabIds)
+
+        assert (areConversionSetsEqual(conversions_loc, true_conversions))
