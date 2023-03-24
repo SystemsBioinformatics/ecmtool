dc84ff7c96575dacf2958204280fa84be3862d40
dhdegroot <daanhugodegroot@gmail.com>
2023-03-24 13:41:18 +0100

    Added more tests

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -4,6 +4,7 @@ import pytest
 import os
 import csv
 from dataclasses import dataclass
+import time
 
 outputfile = os.path.join(os.getcwd(), 'tests', 'test_conversions.csv')
 truth_dir = os.path.join(os.getcwd(), 'tests', 'true_test_results')
@@ -15,12 +16,15 @@ class ConversionsOutput:
     ecoliCoreReturnCode = None
     conversions = None
     metabIds = None
+    computeTime = None
 
     def __init__(self, commandList):
         # Run ecmtool on e coli core network
         if os.path.isfile(outputfile):
             os.remove(outputfile)
+        start = time.time()
         self.ecoliCoreOutput = subprocess.run(commandList, stdout=subprocess.PIPE, text=True)
+        self.computeTime = time.time() - start
 
         self.ecoliCoreReturnCode = self.ecoliCoreOutput.returncode
         with open(outputfile, newline='') as f:
@@ -66,69 +70,227 @@ def areConversionSetsEqual(conversions1, conversions2, atol=1e-8, rtol=1e-5):
 
 class TestEcolicore:
     @pytest.fixture(scope='class', autouse=True)
-    def runEcolicore(self):
+    def runEcmtool(self):
         # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
         commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
                        '--out_path', outputfile]
         ecoliOutput = ConversionsOutput(commandList)
         return ecoliOutput
 
-    def test_ecolicoreRunCompleted(self, runEcolicore):
-        assert (runEcolicore.ecoliCoreReturnCode == 0)
+    def test_ecolicoreRunCompleted(self, runEcmtool):
+        assert (runEcmtool.ecoliCoreReturnCode == 0)
 
-    def test_numberECMs(self, runEcolicore):
-        assert (runEcolicore.conversions.shape[0] == 689)
+    def test_numberECMs(self, runEcmtool):
+        assert (runEcmtool.conversions.shape[0] == 689)
 
-    def test_numberMetabs(self, runEcolicore):
-        assert (len(runEcolicore.metabIds) == 21)
+    def test_numberMetabs(self, runEcmtool):
+        assert (len(runEcmtool.metabIds) == 21)
 
-    def test_metabIds(self, runEcolicore):
+    def test_metabIds(self, runEcmtool):
         trueFilename = os.path.join(truth_dir, 'true_ecolicore_conversions.csv')
         true_metabIds = get_metabs(trueFilename)
-        assert (sorted(true_metabIds) == sorted(runEcolicore.metabIds))
+        assert (sorted(true_metabIds) == sorted(runEcmtool.metabIds))
 
-    def test_conversions(self, runEcolicore):
+    def test_conversions(self, runEcmtool):
         trueFilename = os.path.join(truth_dir, 'true_ecolicore_conversions.csv')
         true_metabIds = get_metabs(trueFilename)
 
         # These true conversions are normalised such that each row sums to 1.
         true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
         # Do the same for the current conversions
-        conversions_loc = runEcolicore.normalize_conversions(true_metabIds)
+        conversions_loc = runEcmtool.normalize_conversions(true_metabIds)
 
         assert areConversionSetsEqual(conversions_loc, true_conversions)
 
+    def test_computeTime(self, runEcmtool):
+        assert runEcmtool.computeTime < 100
 
+
+class TestEcolicoreHide:
+    @pytest.fixture(scope='class', autouse=True)
+    def runEcmtool(self):
+        # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
+        commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
+                       '--out_path', outputfile, '--hide', '54,6,8,14,24,28,29,31,36,38,46,48,62,69']
+        ecoliOutput = ConversionsOutput(commandList)
+        return ecoliOutput
+
+    def test_ecolicoreRunCompleted(self, runEcmtool):
+        assert (runEcmtool.ecoliCoreReturnCode == 0)
+
+    def test_numberECMs(self, runEcmtool):
+        assert (runEcmtool.conversions.shape[0] == 217)
+
+    def test_numberMetabs(self, runEcmtool):
+        assert (len(runEcmtool.metabIds) == 7)
+
+    def test_metabIds(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_hide_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+        assert (sorted(true_metabIds) == sorted(runEcmtool.metabIds))
+
+    def test_conversions(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_hide_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+
+        # These true conversions are normalised such that each row sums to 1.
+        true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
+        # Do the same for the current conversions
+        conversions_loc = runEcmtool.normalize_conversions(true_metabIds)
+
+        assert (areConversionSetsEqual(conversions_loc, true_conversions))
+
+    def test_computeTime(self, runEcmtool):
+        assert runEcmtool.computeTime < 100
+
+
+class TestEcolicoreHideTag:
+    @pytest.fixture(scope='class', autouse=True)
+    def runEcmtool(self):
+        # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
+        commandList = ['python', 'main.py', '--model_path', 'models/sxp_toy.xml', '--auto_direction', 'true', '--tag',
+                       '0', '--out_path', outputfile]
+        ecoliOutput = ConversionsOutput(commandList)
+        ecoliOutput.conversions = ecoliOutput.conversions[None, :]
+        return ecoliOutput
+
+    def test_ecolicoreRunCompleted(self, runEcmtool):
+        assert (runEcmtool.ecoliCoreReturnCode == 0)
+
+    def test_numberECMs(self, runEcmtool):
+        assert (runEcmtool.conversions.shape[0] == 1)
+
+    def test_numberMetabs(self, runEcmtool):
+        assert (len(runEcmtool.metabIds) == 4)
+
+    def test_metabIds(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_sxp_tag_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+        assert (sorted(true_metabIds) == sorted(runEcmtool.metabIds))
+
+    def test_conversions(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_sxp_tag_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+
+        # These true conversions are normalised such that each row sums to 1.
+        true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)[None, :]
+        # Do the same for the current conversions
+        conversions_loc = runEcmtool.normalize_conversions(true_metabIds)
+
+        assert (areConversionSetsEqual(conversions_loc, true_conversions))
+
+    def test_computeTime(self, runEcmtool):
+        assert runEcmtool.computeTime < 10
+
+
+class TestEcolicoreNoCompression:
+    @pytest.fixture(scope='class', autouse=True)
+    def runEcmtool(self):
+        # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
+        commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
+                       '--out_path', outputfile, '--compress', 'false']
+        ecoliOutput = ConversionsOutput(commandList)
+        return ecoliOutput
+
+    def test_ecolicoreRunCompleted(self, runEcmtool):
+        assert (runEcmtool.ecoliCoreReturnCode == 0)
+
+    def test_numberECMs(self, runEcmtool):
+        assert (runEcmtool.conversions.shape[0] == 689)
+
+    def test_numberMetabs(self, runEcmtool):
+        assert (len(runEcmtool.metabIds) == 21)
+
+    def test_metabIds(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_ecolicore_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+        assert (sorted(true_metabIds) == sorted(runEcmtool.metabIds))
+
+    def test_conversions(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_ecolicore_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+
+        # These true conversions are normalised such that each row sums to 1.
+        true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
+        # Do the same for the current conversions
+        conversions_loc = runEcmtool.normalize_conversions(true_metabIds)
+
+        assert areConversionSetsEqual(conversions_loc, true_conversions)
+
+    def test_computeTime(self, runEcmtool):
+        assert runEcmtool.computeTime < 200
+
+
+class TestEcolicoreHideNoCompression:
+    @pytest.fixture(scope='class', autouse=True)
+    def runEcmtool(self):
+        # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
+        commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
+                       '--out_path', outputfile, '--hide', '54,6,8,14,24,28,29,31,36,38,46,48,62,69', '--compress',
+                       'false']
+        ecoliOutput = ConversionsOutput(commandList)
+        return ecoliOutput
+
+    def test_ecolicoreRunCompleted(self, runEcmtool):
+        assert (runEcmtool.ecoliCoreReturnCode == 0)
+
+    def test_numberECMs(self, runEcmtool):
+        assert (runEcmtool.conversions.shape[0] == 217)
+
+    def test_numberMetabs(self, runEcmtool):
+        assert (len(runEcmtool.metabIds) == 7)
+
+    def test_metabIds(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_hide_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+        assert (sorted(true_metabIds) == sorted(runEcmtool.metabIds))
+
+    def test_conversions(self, runEcmtool):
+        trueFilename = os.path.join(truth_dir, 'true_hide_conversions.csv')
+        true_metabIds = get_metabs(trueFilename)
+
+        # These true conversions are normalised such that each row sums to 1.
+        true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
+        # Do the same for the current conversions
+        conversions_loc = runEcmtool.normalize_conversions(true_metabIds)
+
+        assert (areConversionSetsEqual(conversions_loc, true_conversions))
+
+    def test_computeTime(self, runEcmtool):
+        assert runEcmtool.computeTime < 200
+
+
+@pytest.mark.skip(reason="Comment out this skip-mark if you want to test direct intersection method.")
 class TestEcolicoreDirect:
     @pytest.fixture(scope='class', autouse=True)
-    def runEcolicore(self):
+    def runEcmtool(self):
         # global metabIds, conversions, ecoliCoreReturnCode, outputfile, ecoliCoreOutput
         commandList = ['python', 'main.py', '--model_path', 'models/e_coli_core.xml', '--auto_direction', 'true',
                        '--out_path', outputfile, '--direct', 'True']
         ecoliOutput = ConversionsOutput(commandList)
         return ecoliOutput
 
-    def test_ecolicoreRunCompleted(self, runEcolicore):
-        assert (runEcolicore.ecoliCoreReturnCode == 0)
+    def test_ecolicoreRunCompleted(self, runEcmtool):
+        assert (runEcmtool.ecoliCoreReturnCode == 0)
 
-    def test_numberECMs(self, runEcolicore):
-        assert (runEcolicore.conversions.shape[0] == 689)
+    def test_numberECMs(self, runEcmtool):
+        assert (runEcmtool.conversions.shape[0] == 689)
 
-    def test_numberMetabs(self, runEcolicore):
-        assert (len(runEcolicore.metabIds) == 21)
+    def test_numberMetabs(self, runEcmtool):
+        assert (len(runEcmtool.metabIds) == 21)
 
-    def test_metabIds(self, runEcolicore):
+    def test_metabIds(self, runEcmtool):
         trueFilename = os.path.join(truth_dir, 'true_ecolicore_conversions.csv')
         true_metabIds = get_metabs(trueFilename)
-        assert (sorted(true_metabIds) == sorted(runEcolicore.metabIds))
+        assert (sorted(true_metabIds) == sorted(runEcmtool.metabIds))
 
-    def test_conversions(self, runEcolicore):
+    def test_conversions(self, runEcmtool):
         trueFilename = os.path.join(truth_dir, 'true_ecolicore_conversions.csv')
         true_metabIds = get_metabs(trueFilename)
 
         # These true conversions are normalised such that each row sums to 1.
         true_conversions = np.loadtxt(trueFilename, delimiter=',', skiprows=1)
         # Do the same for the current conversions
-        conversions_loc = runEcolicore.normalize_conversions(true_metabIds)
+        conversions_loc = runEcmtool.normalize_conversions(true_metabIds)
 
         assert (areConversionSetsEqual(conversions_loc, true_conversions))
