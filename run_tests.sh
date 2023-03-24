2dbd6065f252ea790f48c53532cd7f6e68a7e91f
Tom <mr@tomclement.nl>
2023-03-24 14:18:58 +0100

    Fixed lrslib installation in docker, run script for tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t ecmtool -f ./docker/Dockerfile .
-docker run ecmtool python3 tests/test_conversions.py
+docker run ecmtool python3 -m pytest tests/test_conversions.py
