0e1a96a3a4523c15e2f232fb8bd9c30e90357853
Tom <mr@tomclement.nl>
2023-03-24 09:21:55 +0100

    Moved integration tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t ecmtool -f ./docker/Dockerfile .
-docker run ecmtool python3 main.py --model_path models/e_coli_core.xml --auto_direction true --direct true --polco true
+docker run ecmtool python3 tests/test_conversions.py
